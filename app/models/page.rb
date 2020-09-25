11ee7a60bef4e650d9bdefc1f6bd73d20c6016a8
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-09-25 21:46:59 +0500

    Apply rubocop rules

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,8 @@
+# Site map node entity
+#
 class Page < ApplicationRecord
   belongs_to :parent, class_name: 'Page', optional: true
-  
+
   validates :path, :name, presence: true
   validates :name, uniqueness: true
-
 end
--- db/migrate/20200925144219_create_pages.rb
+++ db/migrate/20200925144219_create_pages.rb
@@ -1,7 +1,7 @@
 class CreatePages < ActiveRecord::Migration[6.0]
   def change
     create_table :pages do |t|
-      t.references :parent, foreign_key: {to_table: :pages}, index: true
+      t.references :parent, foreign_key: { to_table: :pages }, index: true
       t.string :path
       t.string :name
       t.timestamps
