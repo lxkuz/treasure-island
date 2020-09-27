d0381c76e4262968802799899777c58b5c2a381f
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-09-28 00:48:51 +0500

    Add capybara site_map feature spec with javascript support

---
+++ spec/features/site_map_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Site Map', type: :feature, js: true do
+  include_context 'with seed based data'
+
+  it 'respons site_map structure' do
+    visit root_path
+    wait_a_bit
+    expect(page.body).to include site_map_content
+  end
+
+  private
+
+  def site_map_content
+    load_html_content('site_map')
+  end
+
+  def load_html_content(filename)
+    File.read(Rails.root.join('spec', 'fixtures', "#{filename}.html")).gsub(/>\n*\t*\s*</, '><')
+  end
+
+  def wait_a_bit
+    sleep 1
+  end
+end
